b8bea563844d9b0542bb09cd6c6d673cc6555dcd
nip403 <41174986+nip403@users.noreply.github.com>
2018-07-13 04:09:37 +0100

    Add files via upload

---
+++ Bubble.py
@@ -0,0 +1,43 @@
+import pygame
+
+class Bubble:
+    def __init__(self,displayObject,array,fps,arr_length,accesses,comparisons,clock):
+        self.display = displayObject
+        self.array = array
+        self.fps = fps
+        self.length = arr_length
+        self.accesses = accesses
+        self.comparisons = comparisons
+        self.clock = clock
+
+    def main(self):
+        pos = 0
+        changed = False
+
+        while not changed:
+            self.clock.tick(self.fps)
+
+            try:
+                if self.array[pos] > self.array[pos+1]:
+                    self.array[pos],self.array[pos+1] = self.array[pos+1],self.array[pos]
+                    changed = True
+                pos += 1
+                accesses += 6
+                comparisons += 1
+                
+            except:
+                pos = 0
+
+                if not done:
+                    return
+
+                changed = False
+
+            self.display.events()
+            self.display.draw(self.array,pos)
+            self.display.drawother(self.accesses,self.comparisons)
+
+            pygame.display.flip()
+                
+
+    

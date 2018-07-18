4e8f8da60ca064e9a8e3784843124dc612394a89
nip403 <41174986+nip403@users.noreply.github.com>
2018-07-18 14:37:05 +0800

    Update Cocktail.py

--- Cocktail.py
+++ Cocktail.py
@@ -1,17 +1,16 @@
 import pygame
 
 class Cocktail:
-    def __init__(self,displayObject,array,fps,arr_length,accesses,comparisons,clock):
-        self.display = displayObject
+    def __init__(self,array,displayObject,clock,fps,arr_length):
         self.array = array
-        self.fps = fps
-        self.length = arr_length
-        self.accesses = accesses
-        self.comparisons = comparisons
+        self.display = displayObject
         self.clock = clock
+        self.fps = fps
+        self.accesses = 0
+        self.comparisons = 0
 
     def main(self):
-        for k in range(self.length-1,0,-1):
+        for k in range(len(self.array)-1,0,-1):
             swapped = False
 
             for i in range(k,0,-1):

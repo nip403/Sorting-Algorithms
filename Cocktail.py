7312929ea9c51b1aa4485a80b6cfc6bde242977c
nip403 <41174986+nip403@users.noreply.github.com>
2018-07-18 14:48:39 +0800

    Update Cocktail.py

--- Cocktail.py
+++ Cocktail.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Cocktail:
-    def __init__(self,array,displayObject,clock,fps,arr_length):
+    def __init__(self,array,displayObject,clock,fps):
         self.array = array
         self.display = displayObject
         self.clock = clock

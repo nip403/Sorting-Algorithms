34a11ae00d13b35b7cbcab35d92540de548ddcd2
nip403 <41174986+nip403@users.noreply.github.com>
2018-07-18 14:34:34 +0800

    Update algs.py

--- algs.py
+++ algs.py
@@ -31,41 +31,41 @@ class methods:
         self.comparisons = 0
 
     def bubble(self):
-        method = Bubble(self.array,self.display,self.clock,self.fps,self.accesses,self.comparisons)
+        method = Bubble(self.array,self.display,self.clock,self.fps)
         method.main()
 
     def quicksort(self):
-        method = Quicksort(self.array,self.display,self.clock,self.fps,self.accesses,self.comparisons)
+        method = Quicksort(self.array,self.display,self.clock,self.fps)
         method.main()
 
     def insertion(self):
-        method = Insertion(self.array,self.display,self.clock,self.fps,self.accesses,self.comparisons)
+        method = Insertion(self.array,self.display,self.clock,self.fps)
         method.main()
 
     def cocktail(self):
-        method = Cocktail(self.array,self.display,self.clock,self.fps,self.accesses,self.comparisons)
+        method = Cocktail(self.array,self.display,self.clock,self.fps)
         method.main()
 
     def bogo(self):
-        method = Bogo(self.array,self.display,self.clock,self.fps,self.accesses,self.comparisons)
+        method = Bogo(self.array,self.display,self.clock,self.fps)
         method.main()
 
     def oddeven(self):
-        method = Oddeven(self.array,self.display,self.clock,self.fps,self.accesses,self.comparisons)
+        method = Oddeven(self.array,self.display,self.clock,self.fps)
         method.main()
 
     def shell(self):
-        method = Shell(self.array,self.display,self.clock,self.fps,self.accesses,self.comparisons)
+        method = Shell(self.array,self.display,self.clock,self.fps)
         method.main()
 
     def comb(self):
-        method = Comb(self.array,self.display,self.clock,self.fps,self.accesses,self.comparisons)
+        method = Comb(self.array,self.display,self.clock,self.fps)
         method.main()
 
     def gnome(self):
-        method = Gnome(self.array,self.display,self.clock,self.fps,self.accesses,self.comparisons)
+        method = Gnome(self.array,self.display,self.clock,self.fps)
         method.main()
 
     def merge(self):
-        method = MergeTD(self.array,self.display,self.clock,self.fps,self.accesses,self.comparisons)
+        method = MergeTD(self.array,self.display,self.clock,self.fps)
         mmethod.main()

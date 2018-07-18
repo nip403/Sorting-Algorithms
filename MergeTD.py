026cd828cb361856e6d212dcd76fbc08893c0a13
nip403 <41174986+nip403@users.noreply.github.com>
2018-07-18 14:23:44 +0800

    Update MergeTD.py

--- MergeTD.py
+++ MergeTD.py
@@ -14,8 +14,10 @@ class MergeTD:
         self.clock.tick(self.fps)
 
         if len(array) <= 1:
+            self.comparisons += 1
             return array,i0,i1
 
+        self.accesses += 2
         left,l0,l1 = self.m(array[:len(array)//2],i0,i0+(len(array)//2))
         right,r0,r1 = self.m(array[len(array)//2:],i0+(len(array)//2),i1)
 
@@ -24,6 +26,11 @@ class MergeTD:
         self.accesses += 2
 
         while len(total) > 0:
+            self.clock.tick(self.fps)
+            
+            self.comparisons += 1
+            self.accesses += 5
+
             self.array[l0+len(new)] = min(total)
             new.append(min(total))
 

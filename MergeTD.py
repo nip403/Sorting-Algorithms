0f0dbfbf1a9e8158515cc31e2943944d06e6503b
nip403 <41174986+nip403@users.noreply.github.com>
2018-07-18 13:49:13 +0800

    Update MergeTD.py

--- MergeTD.py
+++ MergeTD.py
@@ -1,139 +1,3 @@
-'''
-package net.bohush.sorting;
-
-import java.awt.Color;
-import java.awt.Graphics;
-
-public class MergeSortPanel extends SortPanel {
-	private static final long serialVersionUID = 1L;
-	private int redColumn = -1;
-	private int blueColumn = -1;
-	private int greenColumnStart = -1;
-	private int greenColumnFinish = -1;
-	
-	public MergeSortPanel(String name, int sleepTime, int width, int height) {
-		super(name, sleepTime, width, height);
-	}
-
-	@Override
-	public void reset() {
-		redColumn = -1;
-		blueColumn = -1;
-		greenColumnStart = -1;
-		greenColumnFinish = -1;
-	}
-
-	@Override
-	public void run() {
-		try {
-			mergeSort(0, list.length - 1);
-			greenColumnStart = 0;
-			greenColumnFinish = size - 1;
-		} catch (InterruptedException e) {
-		}
-		repaint();
-	}
-
-	public void mergeSort(int start, int fin) throws InterruptedException {
-		if ((fin - start) > 0) {
-			mergeSort(start, start + (fin - start) / 2);
-			mergeSort(start + (fin - start) / 2 + 1, fin);
-			merge(start, start + (fin - start) / 2, start + (fin - start) / 2 + 1, fin);
-		}
-	}
-
-	public void merge(int start1, int fin1, int start2, int fin2) throws InterruptedException {
-		int[] list1 = new int[fin1 - start1 + 1];
-		int[] list2 = new int[fin2 - start2 + 1];
-		int[] tmp = new int[list1.length + list2.length];
-		System.arraycopy(list, start1, list1, 0, list1.length);
-		System.arraycopy(list, start2, list2, 0, list2.length);
-		Thread.sleep(2 * sleepTime);
-		repaint();
-	    int current1 = 0;
-	    redColumn = start1 + current1;
-	    int current2 = 0;
-	    blueColumn = start2 + current2;
-	    int current3 = 0;
-
-		while (current1 < list1.length && current2 < list2.length) {
-			Thread.sleep(2 * sleepTime);
-			repaint();
-			if (list1[current1] < list2[current2]) {
-				tmp[current3++] = list1[current1++];
-				redColumn = start1 + current1;
-			} else {
-				tmp[current3++] = list2[current2++];
-				blueColumn = start2 + current2 - 1;
-			}
-			Thread.sleep(2 * sleepTime);
-			repaint();
-		}
-
-		while (current1 < list1.length) {
-			tmp[current3++] = list1[current1++];
-			redColumn = start1 + current1;
-			Thread.sleep(2 * sleepTime);
-			repaint();
-		}
-
-		while (current2 < list2.length) {
-			tmp[current3++] = list2[current2++];
-			blueColumn = start2 + current2 - 1;
-			Thread.sleep(2 * sleepTime);
-			repaint();
-		}
-
-		redColumn = -1;
-		blueColumn = -1;
-		greenColumnStart = start1;
-		for (int i = 0; i < tmp.length; i++) {
-			greenColumnFinish = start1 + i;
-			list[start1 + i] =  tmp[i];
-			Thread.sleep(2 * sleepTime);
-			repaint();
-		}
-		greenColumnStart = -1;
-		greenColumnFinish = -1;
-	}
-
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		int columnWidth = (getWidth() - 4 * BORDER_WIDTH) / size;
-		int columnHeight = (getHeight() - 4 * BORDER_WIDTH) / size;
-		for (int i = 0; i < list.length; i++) {
-			g.setColor(Color.WHITE);
-			g.fillRect(2 * BORDER_WIDTH + columnWidth * i, getHeight() - list[i] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[i] * columnHeight);
-			g.setColor(Color.BLACK);
-			g.drawRect(2 * BORDER_WIDTH + columnWidth * i, getHeight() - list[i] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[i] * columnHeight);			
-		}
-		if((greenColumnStart != -1)&&(greenColumnFinish != -1)) {
-			for (int i = greenColumnStart; i <= greenColumnFinish; i++) {
-				g.setColor(Color.GREEN);
-				g.fillRect(2 * BORDER_WIDTH + columnWidth * i, getHeight() - list[i] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[i] * columnHeight);
-				g.setColor(Color.BLACK);
-				g.drawRect(2 * BORDER_WIDTH + columnWidth * i, getHeight() - list[i] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[i] * columnHeight);			
-			}
-		}
-		if(redColumn != -1) {
-			g.setColor(Color.RED);
-			g.fillRect(2 * BORDER_WIDTH + columnWidth * redColumn, getHeight() - list[redColumn] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[redColumn] * columnHeight);
-			g.setColor(Color.BLACK);
-			g.drawRect(2 * BORDER_WIDTH + columnWidth * redColumn, getHeight() - list[redColumn] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[redColumn] * columnHeight);
-		}
-		if(blueColumn != -1) {
-			g.setColor(Color.BLUE);
-			g.fillRect(2 * BORDER_WIDTH + columnWidth * blueColumn, getHeight() - list[blueColumn] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[blueColumn] * columnHeight);
-			g.setColor(Color.BLACK);
-			g.drawRect(2 * BORDER_WIDTH + columnWidth * blueColumn, getHeight() - list[blueColumn] * columnHeight - 2 * BORDER_WIDTH, columnWidth, list[blueColumn] * columnHeight);
-		}
-
-	}
-
-}
-'''
-
 import pygame
 
 class MergeTD:
@@ -146,36 +10,32 @@ class MergeTD:
         self.comparisons = comparisons
         self.clock = clock
 
-    def m(self,array):
+    def m(self,array,i0,i1):
         self.clock.tick(self.fps)
 
         if len(array) <= 1:
-            return array
+            return array,i0,i1
 
-        left = self.m(array[:len(array)//2])
-        right = self.m(array[len(array)//2:])
+        left,l0,l1 = self.m(array[:len(array)//2],i0,i0+(len(array)//2))
+        right,r0,r1 = self.m(array[len(array)//2:],i0+(len(array)//2),i1)
 
         new = []
         total = left + right
         self.accesses += 2
 
         while len(total) > 0:
+            self.array[l0+len(new)] = min(total)
             new.append(min(total))
+
+            self.display.events()
+            self.display.draw(self.array,i0,i1,l0,l1,r0,r1,l0+len(new))
+            self.display.draw_other(self.accesses,self.comparisons)
+
             del total[total.index(min(total))]
-            self.accesses += 1
 
-        self.display.events()
-        self.display.draw(new)
-        self.display.draw_other(self.accesses,self.comparisons)
+            pygame.display.flip()
 
-        pygame.display.flip()
-
-        return new
+        return new,l0,r1
 
     def main(self):
-        self.display.events()
-        self.display.draw(self.m(self.array))
-        self.display.draw_other(self.accesses,self.comparisons)
-
-        pygame.display.flip()
-        
+        self.m(self.array,0,self.length-1)

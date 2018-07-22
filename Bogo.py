ce41d598ff758858099e82b3207de573e780fe68
nip403 <41174986+nip403@users.noreply.github.com>
2018-07-22 08:21:35 +0800

    Update Bogo.py

--- Bogo.py
+++ Bogo.py
@@ -11,7 +11,7 @@ class Bogo:
         self.comparisons = 0
 
     def main(self):
-        check = lambda arr,acc,com: [all([arr[i+1] > arr[i] for i in range(len(arr)-1)]),2*len(self.array)+acc,len(self.array)+com]
+        check = lambda arr,acc,com: [all([arr[i+1] > arr[i] for i in range(len(arr)-1)]),3*len(self.array)+acc,len(self.array)+com]
 
         while True:
             state,self.accesses,self.comparisons = check(self.array,self.accesses,self.comparisons)

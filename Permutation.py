2b3de97b9807823a0fc56d557e83a6e7e1aed55c
nip403 <41174986+nip403@users.noreply.github.com>
2018-07-22 19:04:07 +0800

    Add files via upload

---
+++ Permutation.py
@@ -0,0 +1,33 @@
+import pygame
+import itertools
+
+class Permutation:
+    def __init__(self,array,displayObject,clock,fps):
+        self.array = array
+        self.display = displayObject
+        self.clock = clock
+        self.fps = fps
+        self.accesses = 0
+        self.comparisons = 0
+
+    def ordered(self,arr):
+        return True if all(x <= arr[i+1] for i,x in enumerate(arr[:-1])) else False
+
+    def main(self):
+        self.accesses += 1
+        for perm in itertools.permutations(self.array):
+            self.clock.tick(self.fps)
+
+            self.accesses += len(perm)-1
+            self.comparisons += len(perm)-2
+
+            self.display.events()
+            self.display.draw(perm,*[i for i in range(len(perm)) if not perm[i] == list(range(len(perm)))[i]])
+            self.display.draw_other(self.accesses,self.comparisons)
+
+            pygame.display.flip()
+
+            if self.ordered(perm):
+                return
+
+            

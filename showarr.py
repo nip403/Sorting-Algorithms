6273d3cbe1612ca8e4268d20b5ddb1cce2b9ca51
nip403 <41174986+nip403@users.noreply.github.com>
2018-07-24 02:55:10 +0800

    Update showarr.py

--- showarr.py
+++ showarr.py
@@ -15,8 +15,8 @@ class Show_array:
             pygame.draw.rect(self.surf,(255,255,255),(p*thick,dimensions[1]-(i/len(self.array)*dimensions[1]),thick,i/length*dimensions[1]),0)
 
     def get_speed_colour(self,length):
-        return [255 if (length/1000*100 if length/1000*100 <= 100 else 100) > 50 else int((percent*2)*255/100), 255 if (length/1000*100 if length/1000*100 <= 100 else 100) < 50 else int(255-(percent*2-100)*255/100),0]
-        
+        return [255 if (length/1000*100 if length/1000*100 <= 100 else 100) > 50 else int(((length/1000*100 if length/1000*100 <= 100 else 100)*2)*255/100), 255 if (length/1000*100 if length/1000*100 <= 100 else 100) < 50 else int(255-((length/1000*100 if length/1000*100 <= 100 else 100)*2-100)*255/100),0]
+ 
     def draw(self,surface,topleft,heading,font):
         surface.blit(self.surf,topleft)
         surface.blit(font.render("Length: %s" % len(self.array),True,self.get_speed_colour(len(self.array))),heading)

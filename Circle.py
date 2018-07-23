838ca32f9d434db3518624aa0f08978a26295787
nip403 <41174986+nip403@users.noreply.github.com>
2018-07-24 00:33:02 +0800

    Update Circle.py

--- Circle.py
+++ Circle.py
@@ -44,6 +44,7 @@ class Circle:
             self.accesses += 4
 
         self.display.events()
+        self.display.add_green([p for p,i in enumerate(self.array) if sorted(self.array)[p] == i])
         self.display.draw(self.array,left,right,left+m,left+m-1)
         self.display.draw_other(self.accesses,self.comparisons)
 

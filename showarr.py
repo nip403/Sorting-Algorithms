0a7ab5a2f8adc2c9088bd0cd4fce0b4519a48030
nip403 <41174986+nip403@users.noreply.github.com>
2018-08-08 23:36:04 +0800

    Added compatibility with drawing dots

--- showarr.py
+++ showarr.py
@@ -2,21 +2,27 @@ import pygame
 import random
 
 class Show_array:
-    def __init__(self,length,dimensions):
+    def __init__(self,length,dimensions,bars):
         self.array = list(range(length))
         random.shuffle(self.array)
 
         self.surf = pygame.Surface(dimensions)
-        self.surf.fill((0,0,0))
-
-        thick = self.surf.get_width()/length
-
-        for p,i in enumerate(self.array):
-            pygame.draw.rect(self.surf,(255,255,255),(p*thick,dimensions[1]-(i/len(self.array)*dimensions[1]),thick,i/length*dimensions[1]),0)
-
+        self.thick = self.surf.get_width()/length
+        self.bars = bars
+        self.length = length
+        self.dimensions = dimensions
+        
     def get_speed_colour(self,length):
         return [255 if (length/1000*100 if length/1000*100 <= 100 else 100) > 50 else int(((length/1000*100 if length/1000*100 <= 100 else 100)*2)*255/100), 255 if (length/1000*100 if length/1000*100 <= 100 else 100) < 50 else int(255-((length/1000*100 if length/1000*100 <= 100 else 100)*2-100)*255/100),0]
  
     def draw(self,surface,topleft,heading,font):
+        self.surf.fill((0,0,0))
+
+        for p,i in enumerate(self.array):
+            if self.bars:
+                pygame.draw.rect(self.surf,(255,255,255),(p*self.thick,self.dimensions[1]-(i/len(self.array)*self.dimensions[1]),self.thick,i/self.length*self.dimensions[1]),0)
+            else:
+                pygame.draw.circle(self.surf,(255,255,255),list(map(int,(p*self.thick,self.dimensions[1]-(i/len(self.array)*self.dimensions[1])))),1,0)
+        
         surface.blit(self.surf,topleft)
         surface.blit(font.render(f"Length: {len(self.array)}",True,self.get_speed_colour(len(self.array))),heading)

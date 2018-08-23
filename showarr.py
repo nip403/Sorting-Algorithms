45d3833dcf3c9acfbd7896d5e36111af9d6c7cad
nip403 <41174986+nip403@users.noreply.github.com>
2018-08-24 02:33:41 +0800

    Update showarr.py

--- showarr.py
+++ showarr.py
@@ -3,15 +3,32 @@ import math
 import random
 
 class Show_array:
-    def __init__(self,length,dimensions,bars):
-        self.array = list(range(length))
-        random.shuffle(self.array)
+    def __init__(self,length,dimensions,bars,mode=0):
+        self.array = self.get_array(length,mode)
 
         self.surf = pygame.Surface(dimensions)
         self.thick = self.surf.get_width()/length
         self.bars = bars
         self.length = length
         self.dimensions = dimensions
+
+    def get_array(self,length,mode=0):
+        arr = list(range(length))
+        
+        if not mode:
+            random.shuffle(arr)
+        elif mode == 2:
+            arr = arr[::-1]
+        elif mode == 3:
+            for i in range(length-1):
+                if random.randint(0,10) < 8:
+                    tmp = random.randint(4,15)
+                    try:
+                        arr[i],arr[i+tmp] = arr[i+tmp],arr[i]
+                    except:
+                        pass
+
+        return arr
         
     def get_speed_colour(self,length):
         return [255 if (length/1000*100 if length/1000*100 <= 100 else 100) > 50 else int(((length/1000*100 if length/1000*100 <= 100 else 100)*2)*255/100), 255 if (length/1000*100 if length/1000*100 <= 100 else 100) < 50 else int(255-((length/1000*100 if length/1000*100 <= 100 else 100)*2-100)*255/100),0]
@@ -21,7 +38,7 @@ class Show_array:
 
         for p,i in enumerate(self.array):
             if self.bars:
-                pygame.draw.rect(self.surf,(255,255,255),(p*self.thick,math.ceil(self.dimensions[1]-(i/len(self.array)*self.dimensions[1])),math.ceil(self.thick),math.ceil(i/self.length*self.dimensions[1])),0)
+                pygame.draw.rect(self.surf,(255,255,255),(p*self.thick,self.dimensions[1]-(i/len(self.array)*self.dimensions[1]),math.ceil(self.thick),math.ceil(i/self.length*self.dimensions[1])),0)
             else:
                 pygame.draw.circle(self.surf,(255,255,255),list(map(int,(p*self.thick,self.dimensions[1]-(i/len(self.array)*self.dimensions[1])))),1,0)
         

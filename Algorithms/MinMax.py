dbd20e497bb2e53f73d309743276cbe56031ec47
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 17:50:59 +0000

    Update MinMax.py

--- Algorithms/MinMax.py
+++ Algorithms/MinMax.py
@@ -3,7 +3,7 @@ import itertools
 import sys
 
 class MinMax:
-    def __init__(self,array,displayObject,clock,fps):
+    def __init__(self, array, displayObject, clock, fps):
         self.array = array
         self.display = displayObject
         self.clock = clock
@@ -23,7 +23,7 @@ class MinMax:
             smallest = 0
             largest = 0
 
-            for p,i in enumerate(self.array):
+            for p, i in enumerate(self.array):
                 self.accesses += 3
                 self.comparisons += 2
                 
@@ -33,9 +33,9 @@ class MinMax:
                     largest = p
 
                 self.display.events()
-                self.display.add_green(list(itertools.chain(range(len(small_end)),range(len(small_end+self.array+large_end)-1,len(small_end+self.array)-1,-1))))
-                self.display.draw(small_end+self.array+large_end,smallest+len(small_end),largest+len(small_end),p+len(small_end))
-                self.display.draw_other(self.accesses,self.comparisons)
+                self.display.add_green(list(itertools.chain(range(len(small_end)), range(len(small_end + self.array + large_end) - 1, len(small_end + self.array) - 1, -1))))
+                self.display.draw(small_end + self.array + large_end, smallest + len(small_end), largest + len(small_end), p + len(small_end))
+                self.display.draw_other(self.accesses, self.comparisons)
 
                 pygame.display.flip()
 
@@ -43,11 +43,11 @@ class MinMax:
             self.comparisons += len(self.array)
             
             small_end.append(self.array[smallest])
-            large_end.insert(0,self.array[largest])
-            self.array = [i for p,i in enumerate(self.array) if not p in [smallest,largest]]
+            large_end.insert(0, self.array[largest])
+            self.array = [i for p, i in enumerate(self.array) if not p in [smallest, largest]]
 
-        self.display.add_green(range(len(small_end+self.array+large_end)))
-        self.display.draw(small_end+self.array+large_end)
-        self.display.draw_other(self.accesses,self.comparisons)
+        self.display.add_green(range(len(small_end + self.array + large_end)))
+        self.display.draw(small_end + self.array + large_end)
+        self.display.draw_other(self.accesses, self.comparisons)
 
         pygame.display.flip()

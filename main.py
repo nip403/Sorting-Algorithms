e7a19c9be06eef586133b80aba64ee6f456f41f8
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 16:07:37 +0000

    Reformatting

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ fps = 0
 draw_bars = True
 
 # Window dimensions
-s = [1000,700]
+s = [1000, 700]
 
 # Initialisations
 pygame.init()
-screen = pygame.display.set_mode(s,0,32)
+screen = pygame.display.set_mode(s, 0, 32)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Sorting Algorithms by NIP")
 
 # Fonts
-bigfont = pygame.font.SysFont("Garamond MS",140)
-medfont = pygame.font.SysFont("Garamond MS",40)
-smallfont = pygame.font.SysFont("Garamond MS",20)
-labels = pygame.font.SysFont("comicsansms",20)
+bigfont = pygame.font.SysFont("Garamond MS", 140)
+medfont = pygame.font.SysFont("Garamond MS", 40)
+smallfont = pygame.font.SysFont("Garamond MS", 20)
+labels = pygame.font.SysFont("comicsansms", 20)
 
 # User Instructions
 def print_instructions():
@@ -44,46 +44,46 @@ def print_instructions():
     print("\n |----                                                    ----| \n")
 
 def main():
-    global arr_length,draw_bars
+    global arr_length, draw_bars
 
     # Initialisations
-    M = methods(fps,clock,screen,smallfont,draw_bars,s)
-    MS = mode_selector([450,470],[100,50],smallfont)
+    M = methods(fps, clock, screen, smallfont, draw_bars, s)
+    MS = mode_selector([450, 470], [100, 50], smallfont)
 
     all_sorts = {
-        "b":["Bubble",M.bubble],
-        "q":["Quick",M.quicksort],
-        "s":["Selection",M.selection],
-        "c":["Cocktail Shaker",M.cocktail],
-        "j":["Bogo",M.bogo],
-        "o":["Odd-Even",M.oddeven],
-        "k":["Shell",M.shell],
-        "l":["Comb",M.comb],
-        "i":["Insertion",M.insertion],
-        "m":["Merge (TopDown)",M.mergetd],
-        "r":["Radix (LSD)",M.radixlsd],
-        "x":["Counting",M.counting],
-        "y":["Cycle",M.cycle],
-        "h":["Heap",M.heap],
-        "w":["Circle",M.circle],
-        "g":["Gnome",M.gnome],
-        "u":["Binary Insertion",M.binaryinsertion],
-        "p":["Pancake",M.pancake],
-        "d":["Permutation",M.permutation],
-        "z":["Strand",M.strand],
-        "v":["Bucket",M.bucket],
-        "e":["MinMax",M.minmax],
-        "t":["Merge (BottomUp)",M.mergebu],
-        "a":["Bitonic",M.bitonic],
-        "f":["Stooge",M.stooge],
-        "n":["Smooth",M.smooth]
+        "b": ["Bubble", M.bubble],
+        "q": ["Quick", M.quicksort],
+        "s": ["Selection", M.selection],
+        "c": ["Cocktail Shaker", M.cocktail],
+        "j": ["Bogo", M.bogo],
+        "o": ["Odd-Even", M.oddeven],
+        "k": ["Shell", M.shell],
+        "l": ["Comb", M.comb],
+        "i": ["Insertion", M.insertion],
+        "m": ["Merge (TopDown)", M.mergetd],
+        "r": ["Radix (LSD)", M.radixlsd],
+        "x": ["Counting", M.counting],
+        "y": ["Cycle", M.cycle],
+        "h": ["Heap", M.heap],
+        "w": ["Circle", M.circle],
+        "g": ["Gnome", M.gnome],
+        "u": ["Binary Insertion", M.binaryinsertion], 
+        "p": ["Pancake", M.pancake],
+        "d": ["Permutation", M.permutation],
+        "z": ["Strand", M.strand],
+        "v": ["Bucket", M.bucket],
+        "e": ["MinMax", M.minmax],
+        "t": ["Merge (BottomUp)", M.mergebu],
+        "a": ["Bitonic", M.bitonic],
+        "f": ["Stooge", M.stooge],
+        "n": ["Smooth", M.smooth]
     }
     
-    head1 = medfont.render("Enter Sorting Type:",True,(140,140,255))
-    head2 = medfont.render("Array:",True,(255,255,255))
+    head1 = medfont.render("Enter Sorting Type:", True, (140, 140, 255))
+    head2 = medfont.render("Array:", True, (255, 255, 255))
 
-    show = Show_array(arr_length,(300,200),draw_bars,MS.state)
-    typelist = [[labels.render(f"{all_sorts[i][0]} Sort: '{i}'",True,(255,145,140)),[120,70+(p*23)]] for p,i in enumerate(all_sorts.keys())]
+    show = Show_array(arr_length, (300, 200), draw_bars, MS.state)
+    typelist = [[labels.render(f"{all_sorts[i][0]} Sort: '{i}'", True, (255, 145, 140)), [120, 70 + (p*23)]] for p, i in enumerate(all_sorts.keys())]
 
     print_instructions()
     current = "b"
@@ -100,10 +100,10 @@ def main():
                 
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:                    
-                    for name,data in all_sorts.items():
+                    for name, data in all_sorts.items():
                         if current == name:
                             pygame.display.set_caption(f"Sorting Algorithms by NIP |||| List size: {arr_length} |||| Sorting method: {data[0]} Sort |||| Sorting mode: {MS.mode}")
-                            M.setup(arr_length,MS.state)
+                            M.setup(arr_length, MS.state)
                             
                             data[1]()
                             pygame.time.wait(500)
@@ -111,15 +111,15 @@ def main():
 
                 elif event.key == pygame.K_BACKSPACE:
                     draw_bars = not draw_bars
-                    setattr(M,"bars",not getattr(M,"bars"))
-                    setattr(show,"bars",not getattr(show,"bars"))
+                    setattr(M, "bars", not getattr(M, "bars"))
+                    setattr(show,"bars", not getattr(show, "bars"))
                     
                 else:
                     current = chr(event.key) if chr(event.key) in "abcdefghijklmnopqrstuvwxyz1234567890" else current
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if MS.change_mode(pygame.mouse.get_pos()):
-                    show = Show_array(arr_length,(300,200),draw_bars,MS.state)
+                    show = Show_array(arr_length, (300, 200), draw_bars, MS.state)
                 
         # User changes array length
         if pygame.key.get_pressed()[pygame.K_UP] or pygame.key.get_pressed()[pygame.K_DOWN] or pygame.key.get_pressed()[pygame.K_RIGHT] or pygame.key.get_pressed()[pygame.K_LEFT]:
@@ -132,29 +132,29 @@ def main():
                 arr_length = 4
 
             # Re-initialise
-            M.setup(arr_length,MS.state)
-            show = Show_array(arr_length,(300,200),draw_bars,MS.state)
+            M.setup(arr_length, MS.state)
+            show = Show_array(arr_length, (300, 200), draw_bars, MS.state)
 
         # Drawing
-        screen.fill((0,0,0))
+        screen.fill((0, 0, 0))
 
-        pygame.draw.rect(screen,(140,140,140),(s[0]/2-100,s[1]/2-100,200,200),10)
-        pygame.draw.rect(screen,(100,100,100),(s[0]/2-90,s[1]/2-90,180,180),0)
+        pygame.draw.rect(screen, (140, 140, 140), (s[0]/2-100, s[1]/2-100, 200, 200), 10)
+        pygame.draw.rect(screen, (100, 100, 100), (s[0]/2 - 90, s[1]/2 - 90, 180, 180), 0)
 
-        for i in [[100,100],[-100,-100],[100,-100],[-100,100]]:
-            pygame.draw.circle(screen,(140,140,140),list(map(int,(s[0]/2+i[0],s[1]/2+i[1]))),4,0)
+        for i in [[100, 100], [-100, -100], [100, -100], [-100, 100]]:
+            pygame.draw.circle(screen, (140, 140, 140), list(map(int, (s[0]/2 + i[0], s[1]/2 + i[1]))), 4, 0)
 
-        screen.blit(head1,(40,30))
-        screen.blit(head2,(750,140))
+        screen.blit(head1, (40, 30))
+        screen.blit(head2, (750, 140))
 
-        show.draw(screen,[640,200],[740,440],labels)
+        show.draw(screen, [640, 200], [740, 440], labels)
         MS.draw(screen)
 
         for i in typelist:
-            screen.blit(i[0],i[1])
+            screen.blit(i[0], i[1])
 
-        letter = bigfont.render(str(current),True,(0,255,0))
-        screen.blit(letter,letter.get_rect(center=[i/2 for i in s]))
+        letter = bigfont.render(str(current), True, (0, 255, 0))
+        screen.blit(letter, letter.get_rect(center=[i/2 for i in s]))
 
         pygame.display.flip()
 

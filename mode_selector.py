e96d6066f4acfb1d9beb75948d28b12d4a73d194
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 16:38:26 +0000

    Reformatting

--- mode_selector.py
+++ mode_selector.py
@@ -1,7 +1,7 @@
 import pygame
 
 class mode_selector:
-    def __init__(self,topleft_pos,dimensions,font):
+    def __init__(self, topleft_pos, dimensions, font):
         self.pos = topleft_pos
         self.dimensions = dimensions
         self.font = font
@@ -14,39 +14,39 @@ class mode_selector:
         ]
 
         self.mode = "Random"
-        self.text = self.font.render(self.mode,True,(0,0,0))
+        self.text = self.font.render(self.mode, True, (0, 0, 0))
         
-        self.rect = pygame.Rect(self.pos,self.dimensions)
+        self.rect = pygame.Rect(self.pos, self.dimensions)
         self.surf = pygame.Surface(self.dimensions)
         self.draw_in()
 
     def draw_in(self):
-        self.surf.fill((30,144,255))
-        pygame.draw.rect(self.surf,(148,0,211),(0,0,*self.dimensions),5)
-        self.surf.blit(self.text,self.text.get_rect(center=[i/2 for i in self.dimensions]))
+        self.surf.fill((30, 144, 255))
+        pygame.draw.rect(self.surf, (148, 0, 211), (0, 0, *self.dimensions), 5)
+        self.surf.blit(self.text, self.text.get_rect(center=[i/2 for i in self.dimensions]))
 
-    def change_mode(self,mouse):
+    def change_mode(self, mouse):
         changed = False
         
-        if pygame.Rect(mouse[0]-1,mouse[1]-1,2,2).colliderect(self.rect):
-            self.mode = self.modes[self.modes.index(self.mode)+1 if self.modes.index(self.mode)+1 < len(self.modes) else 0]
+        if pygame.Rect(mouse[0]-1, mouse[1]-1, 2, 2).colliderect(self.rect):
+            self.mode = self.modes[self.modes.index(self.mode) + 1 if self.modes.index(self.mode) + 1 < len(self.modes) else 0]
             changed = True
             
-        self.text = self.font.render(self.mode,True,(0,0,0))
+        self.text = self.font.render(self.mode, True, (0, 0, 0))
         self.draw_in()
 
         return changed
 
-    def draw(self,surf):
-        surf.blit(self.surf,(self.pos))
+    def draw(self, surf):
+        surf.blit(self.surf, (self.pos))
 
     @property
     def state(self):
         return self.modes.index(self.mode)
 
     @state.setter
-    def state(self,new):
-        if isinstance(new,int) and 0 <= new < 4:
+    def state(self, new):
+        if isinstance(new, int) and 0 <= new < 4:
             self.mode = self.modes[new]
         elif new in self.modes:
             self.mode = new

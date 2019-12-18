8802aeb7abb6a76e08f1ace15269d25e604d927e
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 16:52:10 +0000

    Update BinaryInsertion.py

--- Algorithms/BinaryInsertion.py
+++ Algorithms/BinaryInsertion.py
@@ -1,7 +1,7 @@
 import pygame
 
 class BinaryInsertion:
-    def __init__(self,array,displayObject,clock,fps):
+    def __init__(self, array, displayObject, clock, fps):
         self.array = array
         self.display = displayObject
         self.clock = clock
@@ -13,7 +13,7 @@ class BinaryInsertion:
         self.display.add_green([0])
         self.accesses += 1
         
-        for i in range(1,len(self.array)):
+        for i in range(1, len(self.array)):
             self.clock.tick(self.fps)
             
             key = self.array[i]
@@ -25,7 +25,7 @@ class BinaryInsertion:
             while high > low:
                 self.clock.tick(self.fps)
                 
-                mid = (low+high)//2
+                mid = (low + high)//2
 
                 if self.array[mid] < key:
                     low = mid + 1
@@ -36,8 +36,8 @@ class BinaryInsertion:
                 self.accesses += 1
 
                 self.display.events()
-                self.display.draw(self.array,low,mid,high)
-                self.display.draw_other(self.accesses,self.comparisons)
+                self.display.draw(self.array, low, mid, high)
+                self.display.draw_other(self.accesses, self.comparisons)
 
                 pygame.display.flip()
 
@@ -45,8 +45,8 @@ class BinaryInsertion:
             self.array[:] = self.array[:low] + [key] + self.array[low:i] + self.array[i+1:]
 
             self.display.events()
-            self.display.add_green([i],False)
-            self.display.draw(self.array,low,i,mid,high)
-            self.display.draw_other(self.accesses,self.comparisons)
+            self.display.add_green([i], False)
+            self.display.draw(self.array, low, i, mid, high)
+            self.display.draw_other(self.accesses, self.comparisons)
 
             pygame.display.flip()

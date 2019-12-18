8440e47561954c71988abe229d30d5dc98c141a2
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 16:29:47 +0000

    Reformatting

--- display.py
+++ display.py
@@ -3,10 +3,10 @@ import math
 import sys
 
 class Display:
-    def __init__(self,bar_thickness,windowsize,surface,font):
-        self.base_colour = (255,255,255)
-        self.sorting_colour = (255,0,0)
-        self.done_colour = (0,255,0)
+    def __init__(self, bar_thickness, windowsize, surface, font):
+        self.base_colour = (255, 255, 255)
+        self.sorting_colour = (255, 0, 0)
+        self.done_colour = (0, 255, 0)
         
         self.thickness = bar_thickness
         self.s = windowsize
@@ -15,38 +15,38 @@ class Display:
 
         self.green = []
 
-    def add_green(self,arr,replace=True):
+    def add_green(self, arr, replace=True):
         if replace:
             self.green = arr
         else:
-            self.green = list(set(self.green+arr))
+            self.green = list(set(self.green + arr))
 
-    def draw(self,array,*highlighted,background=(0,0,0)):
+    def draw(self, array, *highlighted, background=(0, 0, 0)):
         self.surface.fill(background)
 
-        for index,item in enumerate(array):
+        for index, item in enumerate(array):
             if self.bars:
                 if index in highlighted:
-                    pygame.draw.rect(self.surface,self.sorting_colour,(index*self.thickness,self.s[1]-(item/len(array)*self.s[1]),math.ceil(self.thickness),math.ceil(item/len(array)*self.s[1])),0)
+                    pygame.draw.rect(self.surface, self.sorting_colour, (index*self.thickness, self.s[1] - (item/len(array) * self.s[1]), math.ceil(self.thickness), math.ceil(item/len(array) * self.s[1])), 0)
                 elif index in self.green:
-                    pygame.draw.rect(self.surface,self.done_colour,(index*self.thickness,self.s[1]-(item/len(array)*self.s[1]),math.ceil(self.thickness),math.ceil(item/len(array)*self.s[1])),0)
+                    pygame.draw.rect(self.surface, self.done_colour, (index*self.thickness, self.s[1] - (item/len(array) * self.s[1]), math.ceil(self.thickness), math.ceil(item/len(array) * self.s[1])), 0)
                 else:
-                    pygame.draw.rect(self.surface,self.base_colour,(index*self.thickness,self.s[1]-(item/len(array)*self.s[1]),math.ceil(self.thickness),math.ceil(item/len(array)*self.s[1])),0)
+                    pygame.draw.rect(self.surface, self.base_colour, (index*self.thickness, self.s[1] - (item/len(array) * self.s[1]), math.ceil(self.thickness), math.ceil(item/len(array) * self.s[1])), 0)
 
             else:
                 if index in highlighted:
-                    pygame.draw.circle(self.surface,self.sorting_colour if not index in highlighted else self.sorting_colour,list(map(int,(index*self.thickness,self.s[1]-(item/len(array)*self.s[1])))),1,0)
+                    pygame.draw.circle(self.surface, self.sorting_colour if not index in highlighted else self.sorting_colour, list(map(int, (index*self.thickness, self.s[1] - (item/len(array) * self.s[1])))), 1, 0)
                 elif index in self.green:
-                    pygame.draw.circle(self.surface,self.done_colour if not index in highlighted else self.sorting_colour,list(map(int,(index*self.thickness,self.s[1]-(item/len(array)*self.s[1])))),1,0)
+                    pygame.draw.circle(self.surface, self.done_colour if not index in highlighted else self.sorting_colour, list(map(int, (index*self.thickness, self.s[1] - (item/len(array) * self.s[1])))), 1, 0)
                 else:
-                    pygame.draw.circle(self.surface,self.base_colour if not index in highlighted else self.sorting_colour,list(map(int,(index*self.thickness,self.s[1]-(item/len(array)*self.s[1])))),1,0)
+                    pygame.draw.circle(self.surface, self.base_colour if not index in highlighted else self.sorting_colour, list(map(int, (index*self.thickness, self.s[1] - (item/len(array) * self.s[1])))), 1, 0)
                    
-    def draw_other(self,accesses,comparisons):
-        a = self.font.render(f"Array Accesses: {accesses}",True,(255,0,0))
-        c = self.font.render(f"Array Comparisions: {comparisons}",True,(255,0,0))
+    def draw_other(self, accesses, comparisons):
+        a = self.font.render(f"Array Accesses: {accesses}", True, (255, 0, 0))
+        c = self.font.render(f"Array Comparisions: {comparisons}", True,(255, 0, 0))
 
-        self.surface.blit(a,a.get_rect(topleft=[10,10]))
-        self.surface.blit(c,c.get_rect(topleft=[10,30]))
+        self.surface.blit(a, a.get_rect(topleft=[10, 10]))
+        self.surface.blit(c, c.get_rect(topleft=[10, 30]))
 
     def events(self):
         for event in pygame.event.get():

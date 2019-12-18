f2d34538a7e53d3fc79c6d6d9e50f87ae4eb524c
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 17:54:14 +0000

    Update Pancake.py

--- Algorithms/Pancake.py
+++ Algorithms/Pancake.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Pancake:
-    def __init__(self,array,displayObject,clock,fps):
+    def __init__(self, array, displayObject, clock, fps):
         self.array = array
         self.display = displayObject
         self.clock = clock
@@ -12,12 +12,12 @@ class Pancake:
     def main(self):
         self.accesses += 1
         
-        for pan in range(len(self.array),1,-1):
+        for pan in range(len(self.array), 1, -1):
             self.clock.tick(self.fps)
 
             self.accesses += 1
             self.comparisons += 1
-            big = max(range(pan),key=self.array.__getitem__)
+            big = max(range(pan), key=self.array.__getitem__)
             
             if not big + 1 == pan:
                 self.accesses += 2
@@ -30,8 +30,8 @@ class Pancake:
                 self.array[:pan] = reversed(self.array[:pan])
 
             self.display.events()
-            self.display.add_green([self.array.index(pan-1)],False)
-            self.display.draw(self.array,*range(pan))
-            self.display.draw_other(self.accesses,self.comparisons)
+            self.display.add_green([self.array.index(pan-1)], False)
+            self.display.draw(self.array, *range(pan))
+            self.display.draw_other(self.accesses, self.comparisons)
 
             pygame.display.flip()

1c5d86c1067665b022255f11767e2e31b65c7b24
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 17:32:32 +0000

    Update Cycle.py

--- Algorithms/Cycle.py
+++ Algorithms/Cycle.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Cycle:
-    def __init__(self,array,displayObject,clock,fps):
+    def __init__(self, array, displayObject, clock, fps):
         self.array = array
         self.display = displayObject
         self.clock = clock
@@ -18,7 +18,7 @@ class Cycle:
             item = self.array[i]
             pos = i
            
-            for j in range(i+1,len(self.array)):
+            for j in range(i+1, len(self.array)):
                 if self.array[j] < item:
                     pos += 1
                     
@@ -28,11 +28,11 @@ class Cycle:
             if pos == i:
                 continue
                 
-            self.array[pos],item = item,self.array[pos]
+            self.array[pos], item = item, self.array[pos]
             self.accesses += 2
 
-            self.display.draw(self.array,pos,i,j)
-            self.display.draw_other(self.accesses,self.comparisons)
+            self.display.draw(self.array, pos, i, j)
+            self.display.draw_other(self.accesses, self.comparisons)
 
             pygame.display.flip()
 
@@ -41,25 +41,25 @@ class Cycle:
                 self.comparisons += 1
                 
                 pos = i
-                for j in range(i+1,len(self.array)):
+                for j in range(i+1, len(self.array)):
                     if self.array[j] < item:
                         pos += 1
 
                     self.accesses += 2
                     self.comparisons += 1
 
-                self.array[pos],item = item,self.array[pos]
+                self.array[pos], item = item, self.array[pos]
                 self.accesses += 1
 
                 self.display.events()
-                self.display.add_green([pos],False)
-                self.display.draw(self.array,i,j,pos)
-                self.display.draw_other(self.accesses,self.comparisons)
+                self.display.add_green([pos], False)
+                self.display.draw(self.array, i, j, pos)
+                self.display.draw_other(self.accesses, self.comparisons)
 
                 pygame.display.flip()
 
         self.display.add_green(range(len(self.array)))
-        self.display.draw(self.array,pos)
-        self.display.draw_other(self.accesses,self.comparisons)
+        self.display.draw(self.array, pos)
+        self.display.draw_other(self.accesses, self.comparisons)
 
         pygame.display.flip()

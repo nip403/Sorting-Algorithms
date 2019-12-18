022a0c7f29f42bf588a742db05761e98056e85ac
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 19:26:31 +0000

    Update Strand.py

--- Algorithms/Strand.py
+++ Algorithms/Strand.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Strand:
-    def __init__(self,array,displayObject,clock,fps):
+    def __init__(self, array, displayObject, clock, fps):
         self.array = array
         self.display = displayObject
         self.clock = clock
@@ -9,7 +9,7 @@ class Strand:
         self.accesses = 0
         self.comparisons = 0
 
-    def merge(self,a,b):
+    def merge(self, a, b):
         out = []
 
         while len(a) and len(b):
@@ -18,9 +18,9 @@ class Strand:
             
             out.append((a if a[0] < b[0] else b).pop(0))
 
-        return out+a+b
+        return out + a + b
 
-    def strand(self,arr):
+    def strand(self, arr):
         i = 0
         s = [arr.pop(0)]
 
@@ -37,16 +37,16 @@ class Strand:
 
         return s
 
-    def make_output(self,correct,remaining):
-        return correct + [i for p,i in enumerate(remaining) if not p in correct]
+    def make_output(self, correct, remaining):
+        return correct + [i for p, i in enumerate(remaining) if not p in correct]
 
     def main(self):
         array = self.array[:]
         out = self.strand(array)
 
         self.display.add_green(range(len(out)))
-        self.display.draw(self.array,*range(len(out),len(array)))
-        self.display.draw_other(self.accesses,self.comparisons)
+        self.display.draw(self.array, *range(len(out), len(array)))
+        self.display.draw_other(self.accesses, self.comparisons)
             
         pygame.display.flip()
 
@@ -55,12 +55,12 @@ class Strand:
             self.comparisons += 1
             
             self.clock.tick(self.fps)
-            out = self.merge(out,self.strand(array))
-            final_output = self.make_output(out,self.array)
+            out = self.merge(out, self.strand(array))
+            final_output = self.make_output(out, self.array)
 
             self.display.events()
             self.display.add_green(range(len(out)))
-            self.display.draw(final_output,*range(len(self.array),len(out)-1,-1))
-            self.display.draw_other(self.accesses,self.comparisons)
+            self.display.draw(final_output, *range(len(self.array), len(out)-1, -1))
+            self.display.draw_other(self.accesses, self.comparisons)
             
             pygame.display.flip()

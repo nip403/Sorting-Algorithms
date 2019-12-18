1a00f1bcd42d056136e19fb16aeb1e462bf78eef
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 19:25:04 +0000

    Update Stooge.py

--- Algorithms/Stooge.py
+++ Algorithms/Stooge.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Stooge:
-    def __init__(self,array,displayObject,clock,fps):
+    def __init__(self, array, displayObject, clock, fps):
         self.array = array
         self.display = displayObject
         self.clock = clock
@@ -9,7 +9,7 @@ class Stooge:
         self.accesses = 0
         self.comparisons = 0
 
-    def sort(self,last,index=0):
+    def sort(self, last, index=0):
         self.accesses += 2
         self.comparisons += 2
         
@@ -17,21 +17,21 @@ class Stooge:
             self.clock.tick(self.fps)
             self.accesses += 4
             
-            self.array[index],self.array[last] = self.array[last],self.array[index]
+            self.array[index], self.array[last] = self.array[last], self.array[index]
 
             self.display.events()
-            self.display.add_green([p for p,i in enumerate(self.array) if i == sorted(self.array)[p]])
-            self.display.draw(self.array,index,last)
-            self.display.draw_other(self.accesses,self.comparisons)
+            self.display.add_green([p for p, i in enumerate(self.array) if i == sorted(self.array)[p]])
+            self.display.draw(self.array, index, last)
+            self.display.draw_other(self.accesses, self.comparisons)
 
             pygame.display.flip()
 
         if last-index > 1:
-            t = int((last-index+1)/3)
+            t = int((last - index + 1)/3)
 
-            self.sort(last-t,index)
-            self.sort(last,index+t)
-            self.sort(last-t,index)
+            self.sort(last-t, index)
+            self.sort(last, index+t)
+            self.sort(last-t, index)
             
     def main(self):
         self.sort(len(self.array)-1)

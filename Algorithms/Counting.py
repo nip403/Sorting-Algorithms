86f9bd9a2f910dbe4e7ca6d7e94ad614b6d80cc7
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 17:30:27 +0000

    Update Counting.py

--- Algorithms/Counting.py
+++ Algorithms/Counting.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Counting:
-    def __init__(self,array,displayObject,clock,fps):
+    def __init__(self, array, displayObject, clock, fps):
         self.array = array
         self.display = displayObject
         self.clock = clock
@@ -14,12 +14,12 @@ class Counting:
         output = [0] * len(self.array)
         self.accesses += 5
        
-        for i in range(0,len(self.array)):
+        for i in range(0, len(self.array)):
             count[self.array[i]] += 1
             self.accesses += 2
 
-        for i in range(1,len(count)):
-            count[i] += count[i - 1]
+        for i in range(1, len(count)):
+            count[i] += count[i-1]
             self.accesses += 2
 
         for i in range(len(self.array)):
@@ -30,8 +30,8 @@ class Counting:
             self.accesses += 6
 
             self.display.events()
-            self.display.add_green([count[self.array[i]]],False)
-            self.display.draw([output[i] if (output[i] or i == 0) else self.array[i] for i in range(len(self.array))],self.array[i],count[self.array[i]])
+            self.display.add_green([count[self.array[i]]], False)
+            self.display.draw([output[i] if (output[i] or i == 0) else self.array[i] for i in range(len(self.array))], self.array[i], count[self.array[i]])
             self.display.draw_other(self.accesses,self.comparisons)
 
             pygame.display.flip()

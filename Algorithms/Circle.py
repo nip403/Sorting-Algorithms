56288f40268e2906e6c6e8985611d9ba8bbd06e9
nip403 <41174986+nip403@users.noreply.github.com>
2019-12-18 17:21:20 +0000

    Update Circle.py

--- Algorithms/Circle.py
+++ Algorithms/Circle.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Circle:
-    def __init__(self,array,displayObject,clock,fps):
+    def __init__(self, array, displayObject, clock, fps):
         self.array = array
         self.display = displayObject
         self.clock = clock
@@ -9,8 +9,8 @@ class Circle:
         self.accesses = 0
         self.comparisons = 0
 
-    def csort_backend(self,left,right):
-        n = right-left
+    def csort_backend(self, left, right):
+        n = right - left
 
         if n < 2:
             return 0
@@ -24,39 +24,39 @@ class Circle:
             self.accesses += 2
             self.comparisons += 1
             
-            if self.array[right-(i+1)] < self.array[left+i]:
-                self.array[right-(i+1)],self.array[left+i] = self.array[left+i],self.array[right-(i+1)]
+            if self.array[right - (i+1)] < self.array[left+i]:
+                self.array[right - (i+1)], self.array[left+i] = self.array[left+i], self.array[right - (i+1)]
                 swaps += 1
                 self.accesses += 4
 
             self.display.events()
-            self.display.draw(self.array,left,right,right-i+1,left+i)
-            self.display.draw_other(self.accesses,self.comparisons)
+            self.display.draw(self.array, left, right, right - (i+1), left+i)
+            self.display.draw_other(self.accesses, self.comparisons)
 
             pygame.display.flip()
 
         self.accesses += 2
         self.comparisons += 2
         
-        if n & 1 and self.array[left+m] < self.array[left+m-1]:
-            self.array[left+m-1],self.array[left+m] = self.array[left+m],self.array[left+m-1]
+        if n & 1 and self.array[left + m] < self.array[left + m-1]:
+            self.array[left + m-1], self.array[left + m] = self.array[left + m], self.array[left + m-1]
             swaps += 1
             self.accesses += 4
 
-        self.display.add_green([p for p,i in enumerate(self.array) if sorted(self.array)[p] == i])
-        self.display.draw(self.array,left,right,left+m,left+m-1)
-        self.display.draw_other(self.accesses,self.comparisons)
+        self.display.add_green([p for p, i in enumerate(self.array) if sorted(self.array)[p] == i])
+        self.display.draw(self.array, left, right, left + m, left + m-1)
+        self.display.draw_other(self.accesses, self.comparisons)
 
         pygame.display.flip()
         
-        return swaps + self.csort_backend(left,left+m) + self.csort_backend(left+m,right)
+        return swaps + self.csort_backend(left, left + m) + self.csort_backend(left + m, right)
 
     def main(self):
         s = 1
-        self.display.add_green([p for p,i in enumerate(self.array) if sorted(self.array)[p] == i])
+        self.display.add_green([p for p, i in enumerate(self.array) if sorted(self.array)[p] == i])
 
         while s:
             self.clock.tick(self.fps)
             
             self.accesses += 1
-            s = self.csort_backend(0,len(self.array))
+            s = self.csort_backend(0, len(self.array))
